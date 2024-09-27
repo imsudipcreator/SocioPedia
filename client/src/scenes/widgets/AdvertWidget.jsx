dc85ec9d2aee14dd23341800cbbe6266e45730e7
imsudipcreator <imsudipmahata@gmail.com>
2024-09-27 14:05:29 +0530

    update AdvertWidget.jsx

--- client/src/scenes/widgets/AdvertWidget.jsx
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -20,7 +20,7 @@ const AdvertWidget = () => {
         width="100%"
         height="auto"
         alt="advert"
-        src="http://localhost:/assets/info4.jpeg"
+        src="https://sociopedia-t3x6.onrender.com/assets/info4.jpeg"
         style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
       />
       <FlexBetween>
